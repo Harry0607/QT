8520328edbc838324ef8e4d50262b922cea9643d
Hardik Sheth <hardiksheth@outlook.com>
2019-03-03 18:33:45 -0600

    changes to main

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,36 +1,56 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include <QSerialPort>
+#include <QApplication>
 #include <QDebug>
+#include <QTextStream>
+#include <QFile>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
 
-QSerialPort serial;
-MainWindow::MainWindow(QWidget *parent) :QMainWindow(parent),
-    ui(new Ui::MainWindow)
+int main(int argc, char *argv[])
 {
-    ui->setupUi(this);
-    //serial =
-    serial.setPortName("COM4");
-    serial.setBaudRate(QSerialPort::Baud9600);
-    serial.setDataBits(QSerialPort::Data8);
-    serial.setParity(QSerialPort::NoParity);
-    serial.setStopBits(QSerialPort::OneStop);
-    serial.setFlowControl(QSerialPort::NoFlowControl);
-    serial.open(QIODevice::ReadWrite);
-    serial.write("ok");
-    //connect(serial,SIGNAL(readyRead()),this,SLOT(serialRecieved()));
+    QApplication a(argc, argv);
 
+    QSerialPort serial;
+    serial.setPortName("/dev/ttyS1");
 
-}
+    if(serial.open(QIODevice::ReadWrite)){
+        //Now the serial port is open try to set configuration
+        if(!serial.setBaudRate(QSerialPort::Baud9600))
+            qDebug()<<serial.errorString();
 
+        if(!serial.setDataBits(QSerialPort::Data8))
+            qDebug()<<serial.errorString();
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-    serial.close();
-}
+        if(!serial.setParity(QSerialPort::NoParity))
+            qDebug()<<serial.errorString();
 
-void MainWindow::serialRecieved()
-{
-    ui->label->setText(serial.readAll());
+        if(!serial.setStopBits(QSerialPort::OneStop))
+            qDebug()<<serial.errorString();
 
+        if(!serial.setFlowControl(QSerialPort::NoFlowControl))
+            qDebug()<<serial.errorString();
+
+        //If any error was returned the serial il corrctly configured
+
+        serial.write("M114 \n");
+        //the serial must remain opened
+
+        if(serial.waitForReadyRead(100)){
+            //Data was returned
+            qDebug()<<"Response: "<<serial.readAll();
+        }else{
+            //No data
+            qDebug()<<"Time out";
+        }
+
+        //I have finish alla operation
+        serial.close();
+    }else{
+        qDebug()<<"Serial COM19 not opened. Error: "<<serial.errorString();
+    }
+
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
